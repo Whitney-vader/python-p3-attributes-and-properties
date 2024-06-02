339b5e7893c82e87d13011cc1752bfa166170055
Whitney_vader <whitney@gmail.com>
2024-06-02 05:35:22 +0300

    Initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = ["Pug", "Husky", "Golden Retriever"]
+
+    def __init__(self, name="Fido", breed=None):
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            self.name = "Fido"
+        else:
+            self.name = name
+
+        if breed is not None and breed not in Dog.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            self.breed = None
+        elif breed is not None:
+            self.breed = breed
+        else:
+            self.breed = "Mutt"
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    APPROVED_JOBS = ["ITC", "Sales", "Engineer"]
+
+    def __init__(self, name="John Doe", job=None):
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            self.name = "John Doe"
+        else:
+            self.name = name.title()
+
+        if job is not None and job not in Person.APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            self.job = None
+        elif job is not None:
+            self.job = job
+        else:
+            self.job = "Unemployed"
